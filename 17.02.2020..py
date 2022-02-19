c6e83b33c7de4267e1cda384866bf876c31c925c
YaRa1312 <superfrench041@gmail.com>
2022-02-19 14:14:39 +0200

    cemau

--- 17.02.2020..py
+++ 17.02.2020..py
@@ -6,11 +6,13 @@ def ask (a, b):
             answer=input ("Is the number greater than " + str((a+b)//2) + "? ")
             if answer=="Yes" or answer=="No":
                 break
+            if answer == '2':
+                print('Dimka molodets cemau')
+            break
         if answer == "Yes":
             ask ((a+b)//2+1, b)
-        if answer == '2'
-            print('Dimka molodets cemau')
-            break
+        
+        
         else:
             ask (a, (a+b)//2)
 ask (1,2)
